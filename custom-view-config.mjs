fd19113f2cd771ea03d24132a950d00216bfb6b5
Felipe Lemos <>
2024-07-19 16:57:40 +0200

    Fix: name

--- custom-view-config.mjs
+++ custom-view-config.mjs
@@ -10,7 +10,7 @@ const config = {
     },
     production: {
       customViewId: 'clysrp6th0023bqy5trwebd54',
-      url: 'https://crm-integration.netlify.app/',
+      url: 'https://crm-integration-felipe.netlify.app',
     },
   },
   oAuthScopes: {
